95cbb0332b3c24e92d646b0813890476230a64c1
web25Lucius <web25Lucius@gmail.com>
2020-07-11 05:35:55 -0600

    fixing Provider- compiled successfully

--- README.md
+++ README.md
@@ -118,9 +118,9 @@ const { products, addItem } = useContext(ProductContext);
 
 - Now that we have refactored our `Products` component to utilize `Context API` let's refactor our `Cart` and `Navigation` Component to use `Context API` as well.
 
-- To start create a new file in our contexts folder named `CartContext.js`, this context is going to be utilized by our `ShoppingCart` and `Navigation` component.
+- To start create a new file in our contexts folder named `ShoppingCartContext.js`, this context is going to be utilized by our `ShoppingCart` and `Navigation` component.
 
-- Inside of our new `CartContext` import `createContext` and create a new context named `CartContext`.
+- Inside of our new `ShoppingCartContext` import `createContext` and create a new context named `CartContext`.
 
 **STEP 5 - Providing data with CartContext**
 
--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, createContext } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
 
@@ -7,26 +7,35 @@ import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
 
+
+//Context 
+const {ShoppingCartContext} = createContext();
+const {ProductContext} = createContext();
+
 function App() {
 	const [products] = useState(data);
 	const [cart, setCart] = useState([]);
 
 	const addItem = item => {
-		// add the given item to the cart
+		setCart({...cart, item})
 	};
 
 	return (
 		<div className="App">
-			<Navigation cart={cart} />
+			<ShoppingCartContext.Provider value={{cart}}>
+				<Navigation />
+				<Route path="/cart">
+				   <ShoppingCart />
+		        </Route>
+			</ShoppingCartContext.Provider>
 
-			{/* Routes */}
-			<Route exact path="/">
-				<Products products={products} addItem={addItem} />
-			</Route>
-
-			<Route path="/cart">
-				<ShoppingCart cart={cart} />
-			</Route>
+			<ProductContext.Provider value={{products, addItem}}>
+				{/* Routes */}
+				<Route exact path="/">
+					<Products />
+				</Route>
+			</ProductContext.Provider>
+			
 		</div>
 	);
 }
